295143361af13019944f08ba2ed7bcdee1ebf5a6
abalsantana <abalsantana01@gmail.com>
2023-10-17 20:39:10 -0300

    Commit initial.

---
+++ src/test/java/com/abalsantana/estoque/EstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abalsantana.estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
